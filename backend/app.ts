df3266547b0a2bf7ccc47549abc789e32a9a0560
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-09-04 13:04:00 +0200

    update: typescript config

--- backend/app.ts
+++ backend/app.ts
@@ -1,22 +1,14 @@
-import { NextFunction, Request, Response } from "express";
-
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const indexRouter = require('./routes/index');
+import express, { NextFunction, Request, Response } from "express";
+import createError from 'http-errors';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
 
 const app = express();
-
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req: Request, res: Response, next: NextFunction) {
