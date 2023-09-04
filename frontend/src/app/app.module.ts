87ac36afc0d9d3c90ec0b711ad9cceba9e8b21d1
Jeyhun Rahimli <rahimlijeyhun77@gmail.com>
2023-09-04 23:11:19 +0200

    add: home, review and imprint pages

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layouts/header/header.component';
 import { FooterComponent } from './layouts/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ReviewComponent } from './pages/review/review.component';
+import { ImprintComponent } from './pages/imprint/imprint.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ReviewComponent,
+    ImprintComponent
   ],
   imports: [
     BrowserModule
